b03b2e603868a7f2d82aba15876c94d24d69e6f6
Matt-1985 <blatsche@web.de>
2020-11-10 18:19:40 +0100

    ♻️ Add explanation

--- app.js
+++ app.js
@@ -32,40 +32,41 @@
 //   console.log("Korrekt!");
 // });
 const inquirer = require("inquirer");
-const masterPassword = "test";
 const fs = require("fs").promises;
-const args = process.argv.slice(2);
-const actualPassword = args[0];
+const args = process.argv.slice(2); //durch slice wird ein neues array umgewandelt für bessere lesbarkeit
+const wantedPWDFor = args[0]; // 0 = erste position im neuen arry die angesteuert wird
+
+const masterPassword = "test";
 
 async function getData() {
-  const promise = await fs.readFile("./db.json", "utf8");
-  const data = await JSON.parse(promise);
+  const promise = await fs.readFile("./db.json", "utf8"); // asyncrone functionen laufen nebeneinander ; "warte bis recht was kommt und dann gibb es nach links und dannn die nächgste Zeile"
+  const data = await JSON.parse(promise); //JSON ist für umwandlung und verarbeitung des Objektes
   return data;
 }
 
-const questions = [
-  {
-    type: "password",
-    name: "password",
-    message: "Enter your password",
-  },
-];
-
 async function validateAccess() {
-  const answers = await inquirer.prompt(questions);
-  if (answers.password !== masterPassword) {
+  const rules = [
+    {
+      type: "password",
+      name: "answer",
+      message: "Enter your password",
+    },
+  ];
+
+  const { answer } = await inquirer.prompt(rules); // questions ist das "questions" aus Zeile 48 // await prompt ist das warten auf die nutzer eingabe  //"password" kopiert "password" asus Zeile 51 (destrcution)
+  if (answer !== masterPassword) {
     console.error("Nice try...try again");
-    return;
+    return; // return bedeutet in diesem zusammenhang das beenden der funktion
   }
   console.log("wow...how'd you managed?");
 
   const passwordSafe = await getData();
 
-  const password = passwordSafe[actualPassword];
+  const password = passwordSafe[wantedPWDFor];
   if (password) {
-    console.log(`${actualPassword}: ${password}`);
+    console.log(`${wantedPWDFor}: ${password}`);
   } else {
-    console.log("Endlevel!");
+    console.log("Password not found...you fraud!");
   }
 }
 

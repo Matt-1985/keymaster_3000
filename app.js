2dc8eae1c4944adf6ce38ce5593a410886cbecf7
Matt-1985 <blatsche@web.de>
2020-11-10 11:29:32 +0100

    Add args to the keymaster

--- app.js
+++ app.js
@@ -12,23 +12,26 @@
 //   readline.close();
 // });
 
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 
-let questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What's your name?",
-  },
-  {
-    type: "password",
-    mask: " *",
-    name: "password",
-    message: "Enter your Password",
-  },
-];
+// let questions = [
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "What's your name?",
+//   },
+//   {
+//     type: "password",
+//     mask: " *",
+//     name: "password",
+//     message: "Enter your Password",
+//   },
+// ];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(`Hi ${answers["name"]}!`);
-  console.log("Korrekt!");
-});
+// inquirer.prompt(questions).then((answers) => {
+//   console.log(`Hi ${answers["name"]}!`);
+//   console.log("Korrekt!");
+// });
+const args = process.argv.slice(2);
+const passwordName = args[0];
+console.log(`You want to know the password of ${passwordName}`);

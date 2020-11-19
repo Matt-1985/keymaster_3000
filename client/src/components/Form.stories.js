7dcb2f85b5ab86224ee791bd8e1e952855905264
Matt-1985 <blatsche@web.de>
2020-11-19 15:25:13 +0100

    Impement Storybook

---
+++ client/src/components/Form.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Form from "./Form";
+
+export default {
+  title: "Form/Item",
+  parameters: { layout: "centered" },
+  component: Form,
+};
+export const FormElement = () => <Form title="matthias" />;
